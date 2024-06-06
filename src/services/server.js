dbea828001add54d47d56bb0c98b5c48b4e86b4c
MaganyAlves <magany.delima@edu.unifl.br>
2024-06-06 12:53:48 -0300

    update serve.js

--- src/services/server.js
+++ src/services/server.js
@@ -10,42 +10,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(routes);
 
-
-
-/*
-app.get('/selectUser', (req, res) => {
-    connection.query('SELECT * FROM usuario', (err, users) => {
-        if (err) return res.send(err);
-        res.send(users);
-    });
-}); 
-
-app.post('/registerUser', (req, res) => {
-        const { nome, email, senha } = req.body;
-        connection.query('INSERT INTO usuario (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senha], (err) => {
-                if (err) return res.send(err);
-                res.send('Usuário inserido com sucesso!');
-        });
-});
-
-app.put('/updateUser/:id', (req, res) => {
-        const { nome, email, senha } = req.body;
-        const { id } = req.params;
-        connection.query('UPDATE usuario SET nome = ?, email = ?, senha = ? WHERE idUsuario = ?', [nome, email, senha, id], (err) => {
-                if (err) return res.send(err);
-                res.send('Usuário atualizado com sucesso!');
-        });
-});
-
-app.delete('/deleteUser/:id', (req, res) => {
-        const { id } = req.params;
-        connection.query('DELETE FROM usuario WHERE idUsuario = ?', [id], (err) => {
-                if (err) return res.send(err);
-                res.send('Usuário deletado com sucesso!');
-        });
-});
-*/
-
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
