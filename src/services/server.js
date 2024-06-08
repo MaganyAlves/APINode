baa904729a8c657492a1435cb43037e5b00602e7
MaganyAlves <magany.delima@edu.unifl.br>
2024-06-08 17:31:50 -0300

    update projeto

--- src/services/server.js
+++ src/services/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from '../routes/routes.js';
+import cors from 'cors';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -7,6 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
